adc5f8bd2aecfaffadee705eac2493e30aaa9395
unknown <yutaagra@gmail.com>
2023-09-16 15:50:22 +0700

    Production: Update background on Hero Section

---
+++ src/containers/Hero.jsx
@@ -0,0 +1,32 @@
+// React
+import { useState } from "react";
+
+// local imports
+import heroImage from "../assets/hero-image.svg";
+import logo from "../assets/logo.svg";
+import cartIcon from "../assets/cart-icon.svg";
+import searchIcon from "../assets/search-icon.svg";
+
+import { heroTitle, heroSubtitle } from "../data";
+
+import FadeIn from "../components/FadeIn";
+import NavLink from "../components/NavLink";
+
+// react icons
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+export default function Hero() {
+  return (
+    <div
+      className="h-screen relative flex 
+      flex-col items-center"
+      style={{
+        background: `url(${heroImage})`,
+        backgroundPosition: "bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
+}
